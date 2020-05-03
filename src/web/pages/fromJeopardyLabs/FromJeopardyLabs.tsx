29fbc3b6c2df41ea0e7eb8d9e6c43c555055ce47
Jeff Steinbok <jeff@steinbok.net>
2020-05-03 13:23:14 -0700

    Bettter Jeffpardy Labs

--- src/web/pages/fromJeopardyLabs/FromJeopardyLabs.tsx
+++ src/web/pages/fromJeopardyLabs/FromJeopardyLabs.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { WebServerApiManager, IApiExecutionContext } from "../../utilities/WebServerApiManager";
-import { ICategory } from "../hostPage/Types";
+import { ICategory, IGameData } from "../hostPage/Types";
 import { TextField } from "@material-ui/core";
 
 interface IFromJeopardyLabsState {
-    categoriesJson: string;
+    categories: ICategory[]
+    gameData: IGameData;
 }
 
 export class FromJeopardyLabs extends React.Component<any, IFromJeopardyLabsState> {
@@ -16,7 +17,8 @@ export class FromJeopardyLabs extends React.Component<any, IFromJeopardyLabsStat
         super(props);
 
         this.state = {
-            categoriesJson: ''
+            gameData: null,
+            categories: []
         }
     }
 
@@ -30,8 +32,14 @@ export class FromJeopardyLabs extends React.Component<any, IFromJeopardyLabsStat
             formData: {},
             json: true,
             success: (results: ICategory[]) => {
+                let validCategories: ICategory[] = [];
+                results.forEach((value, index) => {
+                    if (value.clues.length == 5) {
+                        validCategories.push(value);
+                    }
+                })
                 this.setState({
-                    categoriesJson: JSON.stringify(results, null, 4)
+                    categories: this.state.categories.concat(validCategories)
                 })
             },
             error: null
@@ -39,6 +47,50 @@ export class FromJeopardyLabs extends React.Component<any, IFromJeopardyLabsStat
 
         let wsam: WebServerApiManager = new WebServerApiManager();
         wsam.executeApi(context);
+    }
+
+    generateGameData = () => {
+        let gameData: IGameData = {
+            rounds: [],
+            finalJeffpardyCategory: null
+        }
+
+        let finalJeffpardyCategoryIndex = 0;
+
+        if (this.state.categories.length < 7) {
+            alert("You need at least 7 categories for a game.");
+        }
+        if (this.state.categories.length >= 7) {
+            gameData.rounds.push({
+                id: 0,
+                name: "Jeffpardy",
+                categories: this.state.categories.slice(0, 6)
+            })
+
+            finalJeffpardyCategoryIndex = 6;
+        }
+        if (this.state.categories.length >= 12) {
+            gameData.rounds.push({
+                id: 1,
+                name: "Super Jeffpardy",
+                categories: this.state.categories.slice(6, 12)
+            })
+
+            finalJeffpardyCategoryIndex = 12;
+        }
+
+        let finalJeffpardyCategory = this.state.categories[finalJeffpardyCategoryIndex];
+        gameData.finalJeffpardyCategory = {
+            title: finalJeffpardyCategory.title,
+            comment: finalJeffpardyCategory.comment,
+            airDate: finalJeffpardyCategory.airDate,
+            isAsked: finalJeffpardyCategory.isAsked,
+            hasDailyDouble: finalJeffpardyCategory.hasDailyDouble,
+            clues: finalJeffpardyCategory.clues.slice(0, 1)
+        }
+        this.setState({
+            gameData: gameData
+        });
     }
 
     public render() {
@@ -52,11 +104,30 @@ export class FromJeopardyLabs extends React.Component<any, IFromJeopardyLabsStat
                     onChange={ e => { this.jLabsUrlTemp = e.target.value } } />
                 <br />
                 <button
-                    onClick={ this.loadJeopardyLabs }>Load</button>
+                    onClick={ this.loadJeopardyLabs }>Load Categories</button>
                 <p />
+                <div>
+                    Categories Loaded: { this.state.categories.length }
+                    <ul>
+                        { this.state.categories.map((category, index) => {
+                            return (
+                                <li key={ index }> { category.title } </li>
+                            )
+                        })
+                        }
+                    </ul>
+                </div>
+                <button
+                    onClick={ this.generateGameData }>Generate Game Data</button>
                 <div
                     style={ { fontSize: "0.9rem", fontFamily: "Courier", whiteSpace: "pre-wrap" } }>
-                    { this.state.categoriesJson }
+                    { JSON.stringify(this.state.gameData, (key, value) => {
+                        if (key == "isAsked") return undefined;
+                        else if (key == "isDailyDouble") return undefined;
+                        else if (key == "hasDailyDouble") return undefined;
+                        else if (key == "value") return undefined;
+                        else return value;
+                    }, 4) }
                 </div>
             </div>
         );
